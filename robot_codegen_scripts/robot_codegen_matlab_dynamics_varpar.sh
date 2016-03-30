0d0b003f64a582ad3b7db64d0faa4ba453eff272
Moritz Schappler (IRTPC023) <schappler@irt.uni-hannover.de>
2016-03-30 13:12:40 +0100

    [Robot_Codegen] Funktion für Massenmatrix-Zeitableitung generiert

--- robot_codegen_scripts/robot_codegen_matlab_dynamics_varpar.sh
+++ robot_codegen_scripts/robot_codegen_matlab_dynamics_varpar.sh
@@ -265,6 +265,43 @@ do
     echo "Code in ${quelldat##*/} nicht gefunden."
   fi
 
+
+  # Massenmatrix-Zeitableitung (Gelenke)
+  quelldat=$repo_pfad/codeexport/${robot_name}_inertia_joint_joint_time_derivative_par${dynpar}_matlab.m
+  zieldat=$repo_pfad/codeexport/matlabfcn/${robot_name}_inertiaD_joint_sym_lag_varpar_par${dynpar}.m
+  if [ -f $quelldat ]; then
+    cat $tmp_pfad/robot_matlabtmp_inertiaD_joint_par${dynpar}.head.m > $zieldat
+    printf "%%%%Coder Information\n%%#codegen\n" >> $zieldat
+    cat $tmp_pfad/robot_matlabtmp_assert_q.m >> $zieldat
+    cat $tmp_pfad/robot_matlabtmp_assert_qD.m >> $zieldat
+    cat $tmp_pfad/robot_matlabtmp_assert_mdh.m >> $zieldat
+    cat $tmp_pfad/robot_matlabtmp_assert_m.m >> $zieldat
+    if [ $dynpar == 1 ]; then
+      cat $tmp_pfad/robot_matlabtmp_assert_rcom.m >> $zieldat
+      cat $tmp_pfad/robot_matlabtmp_assert_Ic.m >> $zieldat
+    else
+      cat $tmp_pfad/robot_matlabtmp_assert_mrcom.m >> $zieldat
+      cat $tmp_pfad/robot_matlabtmp_assert_If.m >> $zieldat
+    fi
+    printf "\n%%%% Variable Initialization" >> $zieldat
+    cat $tmp_pfad/robot_matlabtmp_q.m >> $zieldat
+    cat $tmp_pfad/robot_matlabtmp_qD.m >> $zieldat
+    cat $tmp_pfad/robot_matlabtmp_par_mdh.m >> $zieldat
+    cat $tmp_pfad/robot_matlabtmp_par_m.m >> $zieldat
+    if [ $dynpar == 1 ]; then
+      cat $tmp_pfad/robot_matlabtmp_par_rcom.m >> $zieldat
+      cat $tmp_pfad/robot_matlabtmp_par_Ic.m >> $zieldat
+    else
+      cat $tmp_pfad/robot_matlabtmp_par_mrcom.m >> $zieldat
+      cat $tmp_pfad/robot_matlabtmp_par_If.m >> $zieldat
+    fi
+    printf "\n%%%%Symbolic Calculation\n%%From ${quelldat##*/}\n" >> $zieldat
+    cat $quelldat >> $zieldat
+    source robot_codegen_matlabfcn_postprocess.sh $zieldat
+  else
+    echo "Code in ${quelldat##*/} nicht gefunden."
+  fi
+
   # Kinetische Energie (Floating Base)
   if [ $dynpar == 1 ]; then
     quelldat=$repo_pfad/codeexport/${robot_name}_energy_kinetic_floatb_baseframe_par${dynpar}_matlab.m
---
+++ robot_codegen_scripts/tmp/robot_matlabtmp_inertiaD_joint_par2.head.m
@@ -0,0 +1,20 @@
+% Calculate time derivative of joint inertia matrix for
+% %RN%
+% Use Code from Maple symbolic Code Generation
+% 
+% Input:
+% q [%NJ%x1]
+%   Joint Angles [rad]
+% qD [%NJ%x1]
+%   Joint Velocities [rad/s]
+% a_mdh, d_mdh, q_offset_mdh [%NJ%x1]
+%   kinematic parameters
+% m_num_mdh, mrSges_num_mdh, Ifges_num_mdh [%NL%x1]
+%   dynamic parameters (parameter set 2: first moment and inertia about link frame origin)
+% 
+% Output:
+% MqD [%NJ%x%NJ%]
+%   time derivative of inertia matrix
+
+function Mq = %RN%_inertia_joint_sym_lag_varpar_par2(q, qD, ...
+  alpha_mdh, a_mdh, d_mdh, q_offset_mdh, b_mdh, beta_mdh, m_num, rSges_num_mdh, Icges_num_mdh)
