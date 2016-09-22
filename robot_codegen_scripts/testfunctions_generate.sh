f8e687e0f1053350013cda658b6a7b4db8826f48
Moritz Schappler (IRTPC023) <schappler@irt.uni-hannover.de>
2016-09-22 13:15:47 +0100

    Vordefinierte Werte für die Kinematikparameter aus Datei laden und in Testfunktionen berücksichtigen (teilweise notwendig, damit kinematische Zwangsbedingungen funktionieren).

--- robot_codegen_scripts/testfunctions_generate.sh
+++ robot_codegen_scripts/testfunctions_generate.sh
@@ -56,30 +56,64 @@ done
 
 # Parameter-Generierungsskript anpassen (damit nicht MDH-Parameter mit Zufallswerten belegt sind, die einen bestimmten Wert haben sollen)
 zieldat=$testfcn_pfad/${robot_name}_varpar_testfunctions_parameter.m
+printf "\n\n%%%% MDH-Parametereinträge auf Zufallswerte setzen" >> $zieldat
+printf "\n%% Aus robot_matlabtmp_par_mdh.m" >> $zieldat
 cat $tmp_pfad/robot_matlabtmp_par_mdh.m >> $zieldat
+
+
+# Werte für kinematische Zwangsbedingungen in Parameter-Generierungsskript eintragen
+# Setzt auch Zahlenwerte für die Kinematikparameter der MDH-Notation, falls gegeben.
+KCP_dat1=$repo_pfad/robot_codegen_constraints/${robot_name}_kinematic_parameter_values.m
+KCP_dat2=$repo_pfad/codeexport/${robot_name}_kinematic_constraints_symbols_list_matlab.m
+if [ -f $KCP_dat1 ] && [ -f $KCP_dat2 ]; then
+  printf "\n%%%% Werte für kinematische Zwangsbedingungen direkt eintragen\n" >> $zieldat
+  printf "\n%% Aus ${robot_name}_kinematic_parameter_values.m\n" >> $zieldat
+  cat $KCP_dat1 >> $zieldat
+  printf "\n%% Aus ${robot_name}_kinematic_constraints_symbols_list_matlab.m\n" >> $zieldat
+  cat $KCP_dat2 >> $zieldat
+  varname_tmp=`grep "=" $zieldat | tail -1 | sed 's/\(.*\)=.*/\1/'`
+  echo "kintmp = $varname_tmp;" >> $zieldat
+else
+printf "\n\n%%%% Kinematikparameter (Zwangsbedingungen) zufällig setzen." >> $zieldat
+printf "\n\n%% Bei kinematischen Zwangsbedingungen führen Zufallswerte aber wahrscheinlich zur" >> $zieldat
+printf "\n\n%% Verletzung der Zwangsbedingungen" >> $zieldat
+printf "\n\nkintmp = rand($robot_NKCP,1);" >> $zieldat
+fi;
+
+
 # Hänge alle Ausdrücke für die MDH-Parameter an und ersetze die Ergebnisvariable
 # So werden nur die Bestandteile übernommen, die Werte enthalten
+# Null-Einträge werden automatisch zu Null gesetzt.
+printf "\n\n%%%% MDH-Parametereinträge mit vorgegebenen Werten überschreiben" >> $zieldat
+printf "\n%% Aus ${robot_name}_parameters_mdh_d.m\n" >> $zieldat
 cat $repo_pfad/codeexport/${robot_name}_parameters_mdh_d.m >> $zieldat
 varname_tmp=`grep "=" $zieldat | tail -1 | sed 's/\(.*\)=.*/\1/'`
 echo "d_mdh = $varname_tmp;" >> $zieldat
+printf "\n%% Aus ${robot_name}_parameters_mdh_a.m\n" >> $zieldat
 cat $repo_pfad/codeexport/${robot_name}_parameters_mdh_a.m >> $zieldat
 varname_tmp=`grep "=" $zieldat | tail -1 | sed 's/\(.*\)=.*/\1/'`
 echo "a_mdh = $varname_tmp;" >> $zieldat
+printf "\n%% Aus ${robot_name}_parameters_mdh_b.m\n" >> $zieldat
 cat $repo_pfad/codeexport/${robot_name}_parameters_mdh_b.m >> $zieldat
 varname_tmp=`grep "=" $zieldat | tail -1 | sed 's/\(.*\)=.*/\1/'`
 echo "b_mdh = $varname_tmp;" >> $zieldat
+printf "\n%% Aus ${robot_name}_parameters_mdh_beta.m\n" >> $zieldat
 cat $repo_pfad/codeexport/${robot_name}_parameters_mdh_beta.m >> $zieldat
 varname_tmp=`grep "=" $zieldat | tail -1 | sed 's/\(.*\)=.*/\1/'`
 echo "beta_mdh = $varname_tmp;" >> $zieldat
+printf "\n%% Aus ${robot_name}_parameters_mdh_alpha.m\n" >> $zieldat
 cat $repo_pfad/codeexport/${robot_name}_parameters_mdh_alpha.m >> $zieldat
 varname_tmp=`grep "=" $zieldat | tail -1 | sed 's/\(.*\)=.*/\1/'`
 echo "alpha_mdh = $varname_tmp;" >> $zieldat
+printf "\n%% Aus ${robot_name}_parameters_mdh_qoffset.m\n" >> $zieldat
 cat $repo_pfad/codeexport/${robot_name}_parameters_mdh_qoffset.m >> $zieldat
 varname_tmp=`grep "=" $zieldat | tail -1 | sed 's/\(.*\)=.*/\1/'`
 echo "q_offset_mdh = $varname_tmp;" >> $zieldat
+printf "\n%% Aus ${robot_name}_parameters_mdh_v.m\n" >> $zieldat
 cat $repo_pfad/codeexport/${robot_name}_parameters_mdh_v.m >> $zieldat
 varname_tmp=`grep "=" $zieldat | tail -1 | sed 's/\(.*\)=.*/\1/'`
 echo "v_mdh = uint8($varname_tmp);" >> $zieldat
 # Ersetze "_mdh", damit die Variablennamen stimmen
 sed -i "s/_mdh//g" $zieldat
+
 
