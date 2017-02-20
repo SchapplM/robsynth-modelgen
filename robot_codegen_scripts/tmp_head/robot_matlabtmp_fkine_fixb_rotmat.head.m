131413dbf950a947f7a76d7725aebdbf8a235a1a
Moritz Schappler (IRTPC023) <schappler@irt.uni-hannover.de>
2017-02-20 11:51:24 +0100

    [Robot_Codegen] Falschen Kommentarblock in Funktionsvorlage korrigiert.

--- robot_codegen_scripts/tmp_head/robot_matlabtmp_fkine_fixb_rotmat.head.m
+++ robot_codegen_scripts/tmp_head/robot_matlabtmp_fkine_fixb_rotmat.head.m
@@ -1,4 +1,4 @@
-% Calculate matrix of centrifugal and coriolis load on the joints for
+% Calculate forward kinematics (homogenous transformation matrices) for fixed-base
 % %RN%
 % Use Code from Maple symbolic Code Generation
 % 
@@ -10,12 +10,11 @@
 % 
 % Output:
 % T_c_mdh [4x4x%NL%]
-%   homogenious transformation matrices for each body frame (MDH)
-%   1:  mdh base (link 0) -> mdh link 1
+%   homogenous transformation matrices for each body frame (MDH)
+%   1:  mdh base (link 0) -> mdh base link 0 (unit matrix, no information)
 %   ...
-%   %NL%:  mdh base (link 0) -> mdh link %NL%
+%   %NL%:  mdh base (link 0) -> mdh link (%NL%-1)
 
 % %VERSIONINFO%
 
-function T_c_mdh = %FN%(q, ...
-  pkin)
+function T_c_mdh = %FN%(q, pkin)
--- robot_codegen_scripts/tmp_head/robot_matlabtmp_fkine_floatb_eulangrpy_rotmat.head.m
+++ robot_codegen_scripts/tmp_head/robot_matlabtmp_fkine_floatb_eulangrpy_rotmat.head.m
@@ -1,4 +1,4 @@
-% Calculate matrix of centrifugal and coriolis load on the joints for
+% Calculate forward kinematics (homogenous transformation matrices) for floating-base
 % %RN%
 % Use Code from Maple symbolic Code Generation
 % 
@@ -14,11 +14,11 @@
 % 
 % Output:
 % T_c_mdh [4x4x%NL%]
-%   homogenious transformation matrices for each body frame (MDH)
+%   homogenous transformation matrices for each body frame (MDH)
 %   1:  world -> mdh base (link 1)
 %   2:  world -> mdh link 2 (first articulated link)
 %   ...
-%   %NL%:  world -> mdh link %NL% (last articulated link)
+%   %NL%:  world -> mdh link (%NL%-1) (last articulated link)
 
 % %VERSIONINFO%
 
