039309a783ccc3216f24fda2c86588f2106ef9f3
Moritz Schappler (IRTPC023) <schappler@irt.uni-hannover.de>
2017-03-14 09:20:55 +0100

    Dimensionsangaben in Kopfzeile korrigiert.

--- robot_codegen_scripts/tmp_head/robot_matlabtmp_PV2_MPV_transformations_fixb.head.m
+++ robot_codegen_scripts/tmp_head/robot_matlabtmp_PV2_MPV_transformations_fixb.head.m
@@ -12,11 +12,11 @@
 % Output:
 % K [%NMPVFIXB%x(%NJ%*10)]
 %   matrix for linear conversion from inertial parameters to base parameters, [Sousa2014] equ. (38)
-% K_d [%NMPVFIXB%x(%NMPVFIXB%-%NJ%*10)]
+% K_d [%NMPVFIXB%x(%NJ%*10-%NMPVFIXB%)]
 %   matrix for the influence of linear dependant parameters
 % P_b [(%NJ%*10)x%NMPVFIXB%]
 %   Permutation matrix for linear independant inertial parameters, [Sousa2014] equ. (33)
-% P_d [(%NJ%*10)x(%NMPVFIXB%-%NJ%*10)]
+% P_d [(%NJ%*10)x(%NJ%*10-%NMPVFIXB%)]
 %   Permutation matrix for linear dependant inertial parameters, [Sousa2014] equ. (33)
 %
 % Source:
--- robot_codegen_scripts/tmp_head/robot_matlabtmp_PV2_MPV_transformations_floatb_eulangrpy.head.m
+++ robot_codegen_scripts/tmp_head/robot_matlabtmp_PV2_MPV_transformations_floatb_eulangrpy.head.m
@@ -10,13 +10,13 @@
 %   kinematic parameters
 % 
 % Output:
-% K [%NMPVFLOATB%x(%NJ%*10)]
+% K [%NMPVFLOATB%x(%NL%*10)]
 %   matrix for linear conversion from inertial parameters to base parameters, [Sousa2014] equ. (38)
-% K_d [%NMPVFLOATB%x(%NMPVFLOATB%-%NJ%*10)]
+% K_d [%NMPVFLOATB%x(%NL%*10-%NMPVFLOATB%)]
 %   matrix for the influence of linear dependant parameters
-% P_b [(%NJ%*10)x%NMPVFLOATB%]
+% P_b [(%NL%*10)x%NMPVFLOATB%]
 %   Permutation matrix for linear independant inertial parameters, [Sousa2014] equ. (33)
-% P_d [(%NJ%*10)x(%NMPVFLOATB%-%NJ%*10)]
+% P_d [(%NL%*10)x(%NL%*10-%NMPVFLOATB%)]
 %   Permutation matrix for linear dependant inertial parameters, [Sousa2014] equ. (33)
 %
 % Source:
