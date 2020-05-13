fa5a591a4e9e9f74d3a3cd17963bcd938ddd9a51
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-05-13 08:35:58 +0100

    Matlab-Pfad-Initialisierung, damit dieses Repo von anderen Repos leichter gefunden werden kann.

---
+++ hybrdyn_path_init.m
@@ -0,0 +1,6 @@
+% Diese Datei dient nur zum Auffinden dieses Ordners mit dem Befehl
+% `which('hybrdyn_path_init.m')` und zum Hinzufügen des Hauptverzeichnisses 
+% des Repos
+
+this_tb_path = fileparts( mfilename('fullpath') );
+addpath(this_tb_path);
