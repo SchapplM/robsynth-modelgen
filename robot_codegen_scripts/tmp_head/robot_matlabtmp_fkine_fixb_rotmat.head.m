91226b68921adecbf67aba0faa97e308f05cdafe
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-05-14 16:45:47 +0100

    Zusätzliche Ausgabe für Kinematikfunktionen: Gestapelte Transformationsmatrizen als 2D-Array.

--- robot_codegen_scripts/tmp_head/robot_matlabtmp_fkine_fixb_rotmat.head.m
+++ robot_codegen_scripts/tmp_head/robot_matlabtmp_fkine_fixb_rotmat.head.m
@@ -7,14 +7,17 @@
 % %INPUT_PKIN%
 % 
 % Output:
-% T_c_mdh [4x4x(%NJ%+1)]
+% Tc_mdh [4x4x(%NJ%+1)]
 %   homogenous transformation matrices for each (body) frame (MDH)
 %   1:  mdh base (link 0) -> mdh base link 0 (unit matrix, no information)
 %   ...
 %   %NL%:  mdh base (link 0) -> mdh frame (%NL%-1), link (%NL%-1)
 %   ...
 %   %NJ%+1:  mdh base (link 0) -> mdh frame (%NJ%)
+% T_c_stack [(%NJ%+1)*3 x 4]
+%   stacked matrices from Tc_mdh into one 2D array, last row left out.
+%   Last row only contains [0 0 0 1].
 
 % %VERSIONINFO%
 
-function T_c_mdh = %FN%(qJ, pkin)
+function [Tc_mdh, Tc_stack] = %FN%(qJ, pkin)
