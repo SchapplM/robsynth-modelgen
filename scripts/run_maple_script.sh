bac2909226b255a1847effb8d085c94ec64e72ea
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2024-04-16 08:31:58 +0200

    Anpassung an Aufruf aus Matlab über WSL

--- scripts/run_maple_script.sh
+++ scripts/run_maple_script.sh
@@ -12,11 +12,27 @@ mpl_filepath=$1
 mpl_dir=${mpl_filepath%/*}     # Verzeichnis
 mpl_file="${mpl_filepath##*/}" # Dateiname
 
+# Pfad zu dieser Datei (run_maple_script.sh) führt zum Repo
+repo_dir=${0%/*}/..
+# Datei zum Speichern des absolten Pfades zur Maple-Binary
+# Bei WSL wird der Pfad im WSL-Dateisystem angegeben
+mplbin_file=$repo_dir/MAPLE_BIN.env
+
 # Umgebungsvariable prüfen: Pfad zu Maple
 if [ "$MAPLE_BIN" == "" ]; then
-  # Standard-Pfad unter Linux
-  MAPLE_BIN=/opt/maple2022/bin/maple
+  if [ -f $mplbin_file ]; then
+    MAPLE_BIN=`cat $mplbin_file`
+  else
+    echo "Pfad zur Maple-Binary weder in Umgebungsvariable MAPLE_BIN, noch in Linkdatei $mplbin_file gegeben"
+    exit 1
+  fi
 fi
+
+if [ ! -f "$MAPLE_BIN" ]; then
+  echo "Maple-Programmdatei aus Umgebungsvariable MAPLE_BIN existiert nicht: $MAPLE_BIN"
+  exit 1
+fi
+
 maple_path=${MAPLE_BIN%/*}   # Verzeichnis
 maple_exc="${MAPLE_BIN##*/}" # Dateiname
 
@@ -25,6 +41,6 @@ maple_exc="${MAPLE_BIN##*/}" # Dateiname
 # Diese Vorgehensweise ist notwendig, wenn Windows-Maple über ein Bash-Skript aus dem Windows-Subsystem für Linux ausgeführt wird.
 pwd_alt=$(pwd)
 cd "$mpl_dir"
-echo "Run \"$mpl_file\""
+echo "Run \"$mpl_file\" using $MAPLE_BIN"
 nice -n 10 "$maple_path/$maple_exc" -q  <<< "read \"$mpl_file\";"
 cd $pwd_alt
